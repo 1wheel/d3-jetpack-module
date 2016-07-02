e7c7340c377003fdc4ae3027b064aac7c9204a23
Adam Pearce <1wheel@gmail.com>
2016-07-02 17:32:16 -0400

    trying translate

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import {selection} from "d3-selection";
+import {transition} from "d3-transition";
+
+import translateSelection from "./src/translate-selection";
+
+selection.prototype.translate = translateSelection
+
+
 export {default as foo} from "./src/foo";
 export {default as wordwrap} from "./src/wordwrap";
