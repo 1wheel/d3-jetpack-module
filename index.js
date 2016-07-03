574bccce092908499b7b47e9824b14fad064b014
Adam Pearce <1wheel@gmail.com>
2016-07-03 03:06:06 -0400

    adds append

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import {selection} from "d3-selection";
 import {transition} from "d3-transition";
 
 import translateSelection from "./src/translate-selection";
+import append from "./src/append";
 
 selection.prototype.translate = translateSelection
-
+selection.prototype.append = append
 
 export {default as wordwrap} from "./src/wordwrap";
 export {default as parseAttributes} from "./src/parseAttributes";
