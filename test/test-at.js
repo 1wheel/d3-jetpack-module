77a0d7f44a407fe6b324c2cd4fcac90c18b0ed8c
Adam Pearce <1wheel@gmail.com>
2016-07-04 01:16:52 -0400

    adds at

---
+++ test/test-at.js
@@ -0,0 +1,32 @@
+var tape = require('tape'),
+    jetpack = require('../'),
+    jsdom = require('jsdom'),
+    d3 = require('d3-selection');
+
+
+tape('at can look up attributes', function(test) {
+  var document = jsdom.jsdom('<div prop="propVal"></div>');
+
+  var prop = d3.select(document.querySelector('div')).at('prop')
+  test.equal(prop, 'propVal');
+  test.end();
+});
+
+tape('at can set attributes', function(test) {
+  var document = jsdom.jsdom('<div></div>');
+
+  d3.select(document.querySelector('div')).at('prop', 'propVal');
+
+  test.equal(document.querySelector('div').getAttribute('prop'), 'propVal');
+  test.end();
+});
+
+tape('at can set attributes with an object', function(test) {
+  var document = jsdom.jsdom('<div></div>');
+
+  d3.select(document.querySelector('div')).at({prop: 'propVal', width: 100});
+
+  test.equal(document.querySelector('div').getAttribute('prop'), 'propVal');
+  test.equal(document.querySelector('div').getAttribute('width'), '100');
+  test.end();
+});
