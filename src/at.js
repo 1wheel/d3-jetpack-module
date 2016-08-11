0c63a7e0dbe6e33a71170a50dab540b7ee1813f8
Adam Pearce <1wheel@gmail.com>
2016-08-11 18:48:49 -0400

    closes

--- src/at.js
+++ src/at.js
@@ -1,6 +1,6 @@
 export default function(name, value) {
   if (typeof(name) == 'object'){
-    for (var key in name) { this.attr(key, name[key]) }
+    for (var key in name) { this.attr(key.replace('_', ''), name[key]) }
     return this
   } else{
     return arguments.length == 1 ? this.attr(name) : this.attr(name, value)
